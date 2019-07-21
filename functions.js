1997fc35f4d3485dc4ce617144d5b0e2d8253a55
philblanchard <phil@philblanchard.com>
2019-07-21 10:49:18 -0400

    finished: concert search function

--- functions.js
+++ functions.js
@@ -2,6 +2,8 @@ const fs = require('fs')
 const axios = require('axios')
 const chalk = require('chalk')
 require("dotenv").config()
+const parseJson = require('parse-json')
+const moment = require('moment')
 
 
 
@@ -12,13 +14,20 @@ const searchForConcerts = (artist) => {
         }
     })
     .then(function(response){
-        console.log(response);
+        var data = response.data 
+        console.log(chalk.magenta(`${artist} Concert`))
+        for (i in data){
+            console.log(chalk.green('-------------'))
+            console.log(`Venue: ${data[i].venue.name}`)
+            console.log(`Country: ${data[i].venue.country}`)
+            console.log(`City: ${data[i].venue.city}`)
+            console.log(`Date: ${moment(data[i].datetime).format("dddd, MMMM Do YYYY, h:mm:ss a")}`)
+        }
     })
     .catch(function (error){
         console.log(error)
     })
     .then(function () {
-
     })
 }
 
